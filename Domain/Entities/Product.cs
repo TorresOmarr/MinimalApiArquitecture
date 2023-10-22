4a59e187376e0c069e07e12328d6dc462a66a7e2
Omar Torres <jtorres@axsistec.com>
2023-10-22 17:01:17 -0600

    product change

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -2,8 +2,7 @@ namespace Domain.Entities;
 
 public class Product
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public decimal Price { get; set; }
+    public int ProductId { get; set; }
+    public string Description { get; set; } = default!;
+    public double Price { get; set; }
 }
